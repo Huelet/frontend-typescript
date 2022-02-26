8f18c20d19a7da264ccae1c83f02d7ce5a5d6099
Levi <litbelb1@outlook.com>
2022-02-25 22:15:56 -0800

    Signup page updates, linting, and more fixed!

--- pages/auth/up.tsx
+++ pages/auth/up.tsx
@@ -1,78 +1,190 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import Image from "next/image";
+import { SetStateAction, useState } from "react";
 import styles from "../../styles/Signup.module.css";
-import { FontWeights, Text, ColorClassNames } from '@fluentui/react';
+import ReactTooltip from "react-tooltip";
+import { FontWeights, Text, ColorClassNames } from "@fluentui/react";
 
 const AuthUp: NextPage = () => {
-    const [pwgResponse, setPwgResponse] = useState("");
-    function getRandomPassword() {
-        fetch('https://www.random.org/strings/?num=1&len=16&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new')
-            .then(res => res.text())
-            .then(res => {
-                navigator.clipboard.writeText(res);
-                setPwgResponse("Copied to clipboard!");
-            });
-    }
-    return (
-        <div id="klausen">
-            <div className="main-si">
-            <div className="sp-1-eo">
-                <div className="sp-1-io p-5">
-                        <Text styles={{ root: { color: ColorClassNames.white, fontWeight: FontWeights.light, } }}>
-                            <h2 className="klausen-title">Sign up for Huelet!</h2>
-                        </Text>
-                        <Text styles={{ root: { color: ColorClassNames.white, fontWeight: FontWeights.light } }}>
-                            <p className="klausen-subtitle">We only need a username and password </p>
-                        </Text>
-                        <form id="form">
-                            <input className={styles.input} id="username" type="text" name="username" placeholder="Username" />
-                            <div className="spacer-sm"></div>
-                            <div className="pwd-input flex">
-                                <input className={styles.input} id="password" type="password" name="password" placeholder="Password" />
-                                <div className="spacer-sm"></div>
-                                <a href="#pwg-modal">
-                                    <div className={`${styles.pwgTrigger} cursor`}>
-                                        <img src="https://cdn.huelet.net/assets/icons/add.svg" alt="Open password generate modal" />
-                                    </div>
-                                </a>
-                            </div>
-                            <div className="spacer"></div>
-                            <button className={styles.submit} id="submit">Sign up</button>
-                            <div id="error-box"></div>
-                        </form>
-                    </div>
-            </div>
-        </div>
-        <div className={styles.pwgModal} id="pwg-modal">
-            <a href="#">
-                <div className={styles.pwgModalClose}> 
-                    <img src="https://cdn.huelet.net/assets/icons/close.svg" alt="Close password generate modal" />
-                </div>
-            </a>
-            <div className={styles.pwgModalContent}>
-                <div className="sp-1-eo">
-                    <div className="sp-1-io p-5">
-                        <Text styles={{ root: { color: ColorClassNames.white, fontWeight: FontWeights.light, } }}>
-                            <h2>Generate a password</h2>
-                        </Text>
-                        <Text styles={{ root: { color: ColorClassNames.white, fontWeight: FontWeights.light } }}>
-                            <p className="pwg-subtitle">Generate a password for your account</p>
-                        </Text>
-                        <button className={styles.submit} id="pwg-submit" onClick={getRandomPassword}>Generate</button>
-                        <div className="spacer.sm"></div>
-                        <div className="sp-1-io">
-                            <div className="p-5" id="pwg-response">
-                                <Text styles={{ root: { color: ColorClassNames.white } }}>
-                                    {pwgResponse}
-                                </Text>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-    </div>
-    </div>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [accessCode, setAccessCode] = useState("");
+  const [pwgResponse, setPwgResponse] = useState("");
+  const handleUsernameChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setUsername(event.target.value)
+  };
+  const handlePasswordChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setPassword(event.target.value)
+  };
+  const handleAccessCodeChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setAccessCode(event.target.value)
+  };
+  function getRandomPassword() {
+    fetch(
+      "https://www.random.org/strings/?num=1&len=16&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new"
     )
+      .then((res) => res.text())
+      .then((res) => {
+        navigator.clipboard.writeText(res);
+        setPwgResponse("Copied to clipboard!");
+      });
+  }
+  const handleSubmit = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    fetch
+  };
+  return (
+    <div id="klausen">
+      <div className="main-si">
+        <div className="sp-1-eo">
+          <div className="sp-1-io p-5">
+            <Text
+              styles={{
+                root: {
+                  color: ColorClassNames.white,
+                  fontWeight: FontWeights.light,
+                },
+              }}
+            >
+              <h2 className="klausen-title">Sign up for Huelet!</h2>
+            </Text>
+            <Text
+              styles={{
+                root: {
+                  color: ColorClassNames.white,
+                  fontWeight: FontWeights.light,
+                },
+              }}
+            >
+              <p className="klausen-subtitle">
+                We only need a username and password{" "}
+              </p>
+            </Text>
+            <form id="form" onSubmit={handleSubmit}>
+              <input
+                className={styles.input}
+                id="username"
+                type="text"
+                name="username"
+                placeholder="Username"
+                onChange={handleUsernameChange}
+                value={username}
+              />
+              <div className="spacer-sm"></div>
+              <div className="pwd-input flex">
+                <input
+                  className={styles.input}
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={handlePasswordChange}
+                  value={password}
+                />
+                <div className="spacer-sm"></div>
+                <a href="#pwg-modal">
+                  <div className={`${styles.pwgTrigger} cursor`}>
+                    <Image
+                      src={"https://cdn.huelet.net/assets/icons/add.svg"}
+                      alt="Open password generate modal"
+                      width={15}
+                      height={15}
+                      className={styles.pwgTriggerIcon}
+                    />
+                  </div>
+                </a>
+              </div>
+              <div className="spacer-sm"></div>
+              <ReactTooltip />
+              <div className="flex">
+                <input
+                  className={styles.input}
+                  id="accessCode"
+                  type="text"
+                  name="accessCode"
+                  placeholder="Alpha code"
+                  onChange={handleAccessCodeChange}
+                  value={accessCode}
+                />
+                <div
+                  className={"cursor"}
+                  data-tip="We will have sent you a DM with this code. If you don&apos;t have it, please let us know."
+                >
+                  <Image
+                    src={"https://cdn.huelet.net/assets/icons/info.svg"}
+                    alt="Open tooltip"
+                    width={15}
+                    height={15}
+                    className={styles.pwgTriggerIcon}
+                  />
+                </div>
+              </div>
+              <div className="spacer"></div>
+              <button className={styles.submit} id="submit" type="submit">
+                Sign up
+              </button>
+              <div id="error-box"></div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className={styles.pwgModal} id="pwg-modal">
+        <a href="#">
+          <div className={styles.pwgModalClose}>
+            <Image
+              src={"https://cdn.huelet.net/assets/icons/close.svg"}
+              alt="Close password generate modal"
+              width={10}
+              height={10}
+            />
+          </div>
+        </a>
+        <div className={styles.pwgModalContent}>
+          <div className="sp-1-eo">
+            <div className="sp-1-io p-5">
+              <Text
+                styles={{
+                  root: {
+                    color: ColorClassNames.white,
+                    fontWeight: FontWeights.light,
+                  },
+                }}
+              >
+                <h2>Generate a password</h2>
+              </Text>
+              <Text
+                styles={{
+                  root: {
+                    color: ColorClassNames.white,
+                    fontWeight: FontWeights.light,
+                  },
+                }}
+              >
+                <p className="pwg-subtitle">
+                  Generate a password for your account
+                </p>
+              </Text>
+              <button
+                className={styles.submit}
+                id="pwg-submit"
+                onClick={getRandomPassword}
+              >
+                Generate
+              </button>
+              <div className="spacer.sm"></div>
+              <div className="sp-1-io">
+                <div className="p-5" id="pwg-response">
+                  <Text styles={{ root: { color: ColorClassNames.white } }}>
+                    {pwgResponse}
+                  </Text>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthUp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,79 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
 import { Lock, Shield } from "react-feather";
-import { FontSizes, FontWeights, ColorClassNames, Text } from '@fluentui/react';
-import Footer from '../components/footer';
-import Link from 'next/link';
+import { FontSizes, FontWeights, ColorClassNames, Text } from "@fluentui/react";
+import Footer from "../components/footer";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <div id="klausen">
-      <Text styles={{ root: { fontWeight: FontWeights.light, fontSize: FontSizes.size42, color: ColorClassNames.white, textAlign: "center" } }}>
+      <Text
+        styles={{
+          root: {
+            fontWeight: FontWeights.light,
+            fontSize: FontSizes.size42,
+            color: ColorClassNames.white,
+            textAlign: "center",
+          },
+        }}
+      >
         <h2>Hi, we&apos;re Huelet</h2>
       </Text>
-      <Text styles={{ root: { fontWeight: FontWeights.light, fontSize: FontSizes.size32, color: ColorClassNames.white, textAlign: "center" } }}>
-        <h1 className="ms-textAlignCenter ms-fontWeight-light ms-fontSize-68 ms-fontColor-white">A smart video platform for creators, viewers, and advertisers.</h1>
+      <Text
+        styles={{
+          root: {
+            fontWeight: FontWeights.light,
+            fontSize: FontSizes.size32,
+            color: ColorClassNames.white,
+            textAlign: "center",
+          },
+        }}
+      >
+        <h1 className="ms-textAlignCenter ms-fontWeight-light ms-fontSize-68 ms-fontColor-white">
+          A smart video platform for creators, viewers, and advertisers.
+        </h1>
       </Text>
-        <div className="watch-now-clears">
-            <div className="watch-now-link">
-                <Link href="/auth/up" passHref={true}> 
-                    <button className="main-btn">Watch now</button>
-                </Link>
-            </div>
+      <div className="watch-now-clears">
+        <div className="watch-now-link">
+          <Link href="/auth/up" passHref={true}>
+            <button className="main-btn">Watch now</button>
+          </Link>
         </div>
-        <div id="desktop">
-            <div className="main-si">
-                <div className="sp-1-io">
-                    <div className="sp-1-io-inner">
-                        <Lock className="icon-lg privacy-icon" />
-                        <div className="spacer"></div>
-                        <Text styles={{ root: { color: ColorClassNames.white, } }}>
-                            <h2>Privacy</h2>
-                        </Text>
-                    </div>
-                    <Text styles={{ root: { color: ColorClassNames.white } }}>
-                        <p>Click on me to learn more!</p>
-                    </Text>
-                </div>
-                <div className="spacer"></div>
-                <div className="sp-1-io">
-                    <div className="sp-1-io-inner">
-                        <Shield className="icon-lg mod-icon" />
-                        <div className="spacer"></div>
-                        <Text styles={{ root: { color: ColorClassNames.white, } }}>
-                            <h2 className="ms-textAlignCenter ms-fontSize-42">Moderation</h2>
-                        </Text>
-                    </div>
-                    <Text styles={{ root: { color: ColorClassNames.white } }}>
-                        <p>Click on me to learn more!</p>
-                    </Text>
-                </div>
+      </div>
+      <div id="desktop">
+        <div className="main-si">
+          <div className="sp-1-io">
+            <div className="sp-1-io-inner">
+              <Lock className="icon-lg privacy-icon" />
+              <div className="spacer"></div>
+              <Text styles={{ root: { color: ColorClassNames.white } }}>
+                <h2>Privacy</h2>
+              </Text>
             </div>
+            <Text styles={{ root: { color: ColorClassNames.white } }}>
+              <p>Click on me to learn more!</p>
+            </Text>
+          </div>
+          <div className="spacer"></div>
+          <div className="sp-1-io">
+            <div className="sp-1-io-inner">
+              <Shield className="icon-lg mod-icon" />
+              <div className="spacer"></div>
+              <Text styles={{ root: { color: ColorClassNames.white } }}>
+                <h2 className="ms-textAlignCenter ms-fontSize-42">
+                  Moderation
+                </h2>
+              </Text>
+            </div>
+            <Text styles={{ root: { color: ColorClassNames.white } }}>
+              <p>Click on me to learn more!</p>
+            </Text>
+          </div>
         </div>
-            <Footer />
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
