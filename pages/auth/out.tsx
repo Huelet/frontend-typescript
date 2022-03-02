0a99513e5b634374d5b2b858e268f52b4736058f
Levi <litbelb1@outlook.com>
2022-03-01 19:41:43 -0800

    Create logout page

--- pages/auth/out.tsx
+++ pages/auth/out.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
+import { useCookies } from "react-cookie";
 
 const AuthOut: NextPage = () => {
+    const [cookies, setCookie, removeCookie] = useCookies(['_hltoken']);
+    removeCookie('_hltoken');
+    location.assign('/');
     return (
-        <div>Hi, watcha doin here?</div>
+        <div>Busy...</div>
     );
 }
 
