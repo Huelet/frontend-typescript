0228e7466fe982311aef7b79305fed8b4bab335f
Levi <litbelb1@outlook.com>
2022-03-25 22:47:33 -0700

    Update header & remove fluentui icons

--- components/header.tsx
+++ components/header.tsx
@@ -1,54 +1,64 @@
 import next from "next";
+import Link from "next/link";
 import styles from "../styles/Header.module.css";
-import {
-  GlobalNavButtonIcon,
-  SettingsIcon,
-  VideoIcon,
-} from "@fluentui/react-icons-mdl2";
 
 const Header = () => {
-    return (
-        <div className={styles.navContainer}>
-        <div className={styles.navBar}>
-          <GlobalNavButtonIcon className={styles.navIcon} />
-          <div>
-            <form action="" method="get">
-              <input
-                className={styles.searchBar}
-                type="text"
-                name="query"
-                placeholder="Search"
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.navBar}>
+        <img
+          src="https://cdn.huelet.net/assets/icons/menu.svg"
+          alt="menu icon"
+          className={styles.navIcon}
+        />
+        <div>
+          <form action="" method="get">
+            <input
+              className={styles.searchBar}
+              type="text"
+              name="query"
+              placeholder="Search"
+            />
+            <button className={`${styles.searchbutton} cursor`} type="submit">
+              <img
+                src="https://cdn.huelet.net/assets/icons/search.svg"
+                className={styles.searchbutton}
               />
-              <button className={`${styles.searchbutton} cursor`} type="submit">
-                <img
-                  src="https://cdn.huelet.net/assets/icons/search.svg"
-                  className={styles.searchbutton}
-                />
-              </button>
-            </form>
+            </button>
+          </form>
+        </div>
+        <div className={styles.accountIconsContainer}>
+          <div className="hover cursor">
+            <a href="https://dash.huelet.net">
+              <img
+                src="https://cdn.huelet.net/assets/icons/video-camera.svg"
+                alt="video camera icon"
+                className={styles.navIcon}
+              />
+            </a>
           </div>
-          <div className={styles.accountIconsContainer}>
-            <div className="hover cursor">
-              <a href="https://dash.huelet.net">
-                <VideoIcon className={styles.navIcon} />
-              </a>
-            </div>
-            <div className="settings--container hover cursor">
-              <SettingsIcon className={styles.navIcon} />
-            </div>
-            <div className="avatar--container hover cursor">
-              <a href="/auth/manage">
-                <img
-                  className="avatar"
-                  src="https://cdn.huelet.net/assets/AvatarMenu_defaultAvatarSmall.png"
-                  alt="Default profile picture"
-                />
-              </a>
-            </div>
+          <div className="settings--container hover cursor">
+            <Link href="/auth/settings">
+              <img
+                src="https://cdn.huelet.net/assets/icons/settings.svg"
+                alt="settings icon"
+                className={styles.navIcon}
+              />
+            </Link>
+          </div>
+          <div className="avatar--container hover cursor">
+            <a href="/auth/manage">
+              <img
+                className="avatar"
+                src="https://cdn.huelet.net/assets/AvatarMenu_defaultAvatarSmall.png"
+                alt="Default profile picture"
+              />
+            </a>
           </div>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Header;
