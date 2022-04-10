0a1f0717eceadf836021f1f3c496feeb402d3bf7
Levi <litbelb1@outlook.com>
2022-04-09 23:30:01 -0700

    Add ripple loader to video page

--- pages/w/[vuid].tsx
+++ pages/w/[vuid].tsx
@@ -6,8 +6,11 @@ import { Header } from "../../components/header";
 import styles from "../../styles/Video.module.css";
 import Link from "next/link";
 import { useCookies } from "react-cookie";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const ViewVideo: NextPage = () => {
+  const [loading, setLoading] = useState(true);
   const [cookie, setCookie] = useCookies(["_hltoken"]);
   const [username, setUsername] = useState("");
   const [url, setUrl] = useState("");
@@ -36,6 +39,7 @@ const ViewVideo: NextPage = () => {
     fetch(`https://api.huelet.net/videos/${vuid}`)
       .then((resp: Response) => resp.json())
       .then((data: any) => {
+        console.log(data);
         setUrl(data.vurl);
         setTitle(data.vtitle);
         // date tends to have issues so i added a catch, please do not edit this!
@@ -44,12 +48,15 @@ const ViewVideo: NextPage = () => {
         setUpvotes(data.vclaps);
         setDownvotes(data.vcraps);
         setShares(data.vshares);
-        fetch(`https://api.huelet.net/auth/user/${data.vauthor}`)
-          .then((resp: Response) => resp.json())
-          .then((data: any) => {
-            setAuthorId(data.uid);
-            setAuthorUsername(data.username);
-          });
+        setAuthorId(data.vauthor);
+        setTimeout(() => {
+          fetch(`https://api.huelet.net/auth/user/${data.authorId}`)
+            .then((resp: Response) => resp.json())
+            .then((data: any) => {
+              setAuthorUsername(data.username);
+            });
+            setLoading(false);
+        }, 100);
       });
   }
   const addClap = async () => {
@@ -92,97 +99,124 @@ const ViewVideo: NextPage = () => {
     );
     console.log(resp);
   };
-  getData();
   getUserData();
+  getData();
   return (
-    <div id="klausen">
-      <Header />
-      <div className="spacer-sm"></div>
-      <article>
-        <div className={styles.videoWrapper}>
-          <Player src={url} playsInline className={styles.videoPlayer} />
-        </div>
-        <div className={`${styles.videoDetails}`}>
-          <h1 className={`${styles.videoDetailsTitle}`}>{title}</h1>
-          <div className={`${styles.videoDetailsOptions}`}>
-            <div
-              className={`${styles.videoDetailsOptionsReactionPositive} ${styles.videoDetailsOptionsReaction} ${styles.videoDetailsOptionsReactionActive}`}
-              style={{
-                backgroundColor: "#436072",
-              }}
-              onClick={addClap}
-            >
-              <img
-                src="https://cdn.huelet.net/assets/emoji/1F44F.svg"
-                alt="Yellow hands clapping"
-                className={`${styles.videoDetailsOptionsReactionPositiveImg}`}
-                draggable="false"
-                data-type="emoji"
-              ></img>
-              <span>{upvotes}</span>
+    <SkeletonTheme baseColor="#4E4E4E" highlightColor="#686868">
+      <div id="klausen">
+        <Header username={username} />
+        <div className="spacer-sm"></div>
+        <article>
+          {loading ? (
+            <Skeleton width={800} height={430} />
+          ) : (
+            <div className={styles.videoWrapper}>
+              <Player src={url} playsInline className={styles.videoPlayer} />
             </div>
-            <div
-              className={`${styles.videoDetailsOptionsReactionNegative} ${styles.videoDetailsOptionsReaction}`}
-              onClick={addCrap}
-            >
-              <img
-                src="https://cdn.huelet.net/assets/emoji/1F4A9.svg"
-                alt="Brown poop"
-                className={`${styles.videoDetailsOptionsReactionNegativeImg}`}
-                draggable="false"
-                data-type="emoji"
-              ></img>
-              <span>{downvotes}</span>
+          )}
+          <div className={`${styles.videoDetails}`}>
+            <h1 className={`${styles.videoDetailsTitle}`}>{title}</h1>
+            <div className={`${styles.videoDetailsOptions}`}>
+              {loading ? (
+                <Skeleton width={50} height={40} />
+              ) : (
+                <div
+                  className={`${styles.videoDetailsOptionsReactionPositive} ${styles.videoDetailsOptionsReaction} ${styles.videoDetailsOptionsReactionActive}`}
+                  style={{
+                    backgroundColor: "#436072",
+                  }}
+                  onClick={addClap}
+                >
+                  <img
+                    src="https://cdn.huelet.net/assets/emoji/1F44F.svg"
+                    alt="Yellow hands clapping"
+                    className={`${styles.videoDetailsOptionsReactionPositiveImg}`}
+                    draggable="false"
+                    data-type="emoji"
+                  ></img>
+                  <span>{upvotes}</span>
+                </div>
+              )}
+              {loading ? (
+                <Skeleton width={50} height={40} />
+              ) : (
+                <div
+                  className={`${styles.videoDetailsOptionsReactionNegative} ${styles.videoDetailsOptionsReaction}`}
+                  onClick={addCrap}
+                >
+                  <img
+                    src="https://cdn.huelet.net/assets/emoji/1F4A9.svg"
+                    alt="Brown poop"
+                    className={`${styles.videoDetailsOptionsReactionNegativeImg}`}
+                    draggable="false"
+                    data-type="emoji"
+                  ></img>
+                  <span>{downvotes}</span>
+                </div>
+              )}
+              {loading ? (
+                <Skeleton width={50} height={40} />
+              ) : (
+                <div
+                  className={`${styles.videoDetailsOptionsShare} ${styles.videoDetailsOptionsReaction}`}
+                >
+                  <img
+                    src="https://cdn.huelet.net/assets/icons/forward.svg"
+                    alt="A forward-facing arrow"
+                    className={`${styles.videoDetailsOptionsShareImg}`}
+                    draggable="false"
+                    data-type="icon"
+                  ></img>
+                  <span>{shares}</span>
+                </div>
+              )}
             </div>
-
-            <div
-              className={`${styles.videoDetailsOptionsShare} ${styles.videoDetailsOptionsReaction}`}
-            >
-              <img
-                src="https://cdn.huelet.net/assets/icons/forward.svg"
-                alt="A forward-facing arrow"
-                className={`${styles.videoDetailsOptionsShareImg}`}
-                draggable="false"
-                data-type="icon"
-              ></img>
-              <span>{shares}</span>
+            {loading ? (
+              <Skeleton width={870} height={50} />
+            ) : (
+              <Link href={`https://huelet.net/c/${authorId}`} passHref>
+                <h2 className={`${styles.videoDetailsAuthor}`}>
+                  By {authorUsername}
+                </h2>
+              </Link>
+            )}
+            {loading ? (
+              <Skeleton width={100} height={20} />
+            ) : (
+            <div className={styles.videoAttributes}>
+              <span className={`${styles.videoAttributes1}`}>
+                Uploaded: {uploadedDate}
+              </span>
+              <span className={`${styles.videoAttributesSpacerText}`}>
+                &nbsp;|&nbsp;
+              </span>
+              <span className={`${styles.videoAttributes2}`}>
+                Views: {views}
+              </span>
             </div>
+            )}
           </div>
-          <Link href={`https://huelet.net/c/${authorId}`} passHref>
-            <h2 className={`${styles.videoDetailsAuthor}`}>
-              By {authorUsername}
-            </h2>
-          </Link>
-          <div className={styles.videoAttributes}>
-            <span className={`${styles.videoAttributes1}`}>
-              Uploaded: {uploadedDate}
-            </span>
-            <span className={`${styles.videoAttributesSpacerText}`}>
-              &nbsp;|&nbsp;
-            </span>
-            <span className={`${styles.videoAttributes2}`}>Views: {views}</span>
-          </div>
-        </div>
-        <div className={`${styles.videoComments}`}>
-          <h2 className={`${styles.videoCommentsHeading}`}>Comments</h2>
-          <div className={`${styles.videoCommentsBox}`}>
-            <textarea className={`${styles.videoCommentsBoxText}`}></textarea>
-            <div className={`${styles.videoCommentsBoxSubmit}`}>
-              <button
-                type="submit"
-                className={`${styles.videoCommentsBoxSubmitButton}`}
-              >
-                Post
-              </button>
+          <div className={`${styles.videoComments}`}>
+            <h2 className={`${styles.videoCommentsHeading}`}>Comments</h2>
+            <div className={`${styles.videoCommentsBox}`}>
+              <textarea className={`${styles.videoCommentsBoxText}`}></textarea>
+              <div className={`${styles.videoCommentsBoxSubmit}`}>
+                <button
+                  type="submit"
+                  className={`${styles.videoCommentsBoxSubmitButton}`}
+                >
+                  Post
+                </button>
+              </div>
             </div>
+            <div></div>
           </div>
-          <div></div>
-        </div>
-      </article>
-      <aside>
-        <div className="adFrame--rightAsideColumn">hi!! i&apos;m an ad!</div>
-      </aside>
-    </div>
+        </article>
+        <aside>
+          <div className="adFrame--rightAsideColumn">hi!! i&apos;m an ad!</div>
+        </aside>
+      </div>
+    </SkeletonTheme>
   );
 };
 
