e8b3ec6ef001b0cbe911aa999ce517ac4c5529f9
Levi <litbelb1@outlook.com>
2022-04-14 18:39:13 -0700

    Add cookie consent form (beta)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,15 @@ import "../styles/adstyles.css";
 import "../styles/VideoPlayer.css";
 import type { AppProps } from "next/app";
 import { CookiesProvider } from "react-cookie";
+import { ConsentGate, ConfirmicProvider } from "@confirmic/react";
 
 function HueletWebapp({ Component, pageProps }: AppProps) {
   return (
-    <CookiesProvider>
-      <Component {...pageProps} />
-    </CookiesProvider>
+    <ConfirmicProvider projectId="prj:7466fc15-caeb-4765-a170-24e302583964" debug={true}>
+      <CookiesProvider>
+        <Component {...pageProps} />
+      </CookiesProvider>
+    </ConfirmicProvider>
   );
 }
 
