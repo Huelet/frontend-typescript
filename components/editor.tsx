4995b1e7c6f506a234019375f2e00af962a70b6a
Levi Richardson <litbelb1@outlook.com>
2022-07-11 01:33:40 +0000

    🐛 stop using milkdown as editor

--- components/editor.tsx
+++ components/editor.tsx
@@ -1,27 +1,13 @@
 import React, { Dispatch, SetStateAction } from "react";
-import { listener, listenerCtx } from "@milkdown/plugin-listener";
-import { Editor, rootCtx } from "@milkdown/core";
-import { nord } from "@milkdown/theme-nord";
-import { ReactEditor, useEditor } from "@milkdown/react";
-import { commonmark } from "@milkdown/preset-commonmark";
 
 export interface EditorProps {
+  chonky: boolean;
+  value: string;
   output: Dispatch<SetStateAction<string>>;
 }
 
-export const MilkdownEditor = ({ output }: EditorProps) => {
-  const editor = useEditor((root) =>
-    Editor.make()
-      .config((ctx) => {
-        ctx.set(rootCtx, root);
-        ctx.get(listenerCtx).markdownUpdated((ctx, markdown, prevMarkdown) => {
-          output(markdown);
-        });
-      })
-      .use(nord)
-      .use(listener)
-      .use(commonmark)
-  );
-
-  return <ReactEditor editor={editor} />;
+export const MilkdownEditor = ({ value, chonky, output }: EditorProps) => {
+  return (
+    <div></div>
+  )
 };
