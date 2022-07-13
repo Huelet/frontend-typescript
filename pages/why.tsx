acbbf0f01f6143df9b8dd8e42b8527fc9ba6b171
Levi Richardson <litbelb1@outlook.com>
2022-07-13 04:43:08 +0000

    🥚 here you go sam

---
+++ pages/why.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import { Card } from "../components/card";
+
+const Why: NextPage = () => {
+  return (
+    <div id="klausen">
+      <Card full={true}>
+        <button>this button does absolutely nothing</button>
+      </Card>
+    </div>
+  );
+};
+
+export default Why;
