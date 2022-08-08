57c919a80041bdfaea77ede403f08d64705bbac0
Levi R <levir620@skiff.com>
2022-08-08 16:45:08 -0700

    ⚡️ useEffect on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,37 @@ import * as React from "react";
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { useCookies } from "react-cookie";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
 	const [cookie, setCookie] = useCookies(["_hltoken"]);
-	const checkCookie = async () => {
-		const token = cookie._hltoken;
-		if (token) {
-			const resp = await fetch("https://api.huelet.net/auth/token", {
-				method: "GET",
-				mode: "cors",
-				cache: "no-cache",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${token}`,
-				},
-			});
-			const data = await resp.json();
-			console.log(data);
-			if (resp.status === 200) {
+	const router = useRouter();
+
+	React.useEffect(() => {
+		const checkCookie = async () => {
+			const token = cookie._hltoken;
+			if (token) {
+				const resp = await fetch("https://api.huelet.net/auth/token", {
+					method: "GET",
+					mode: "cors",
+					cache: "no-cache",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${token}`,
+					},
+				});
+				const data = await resp.json();
 				console.log(data);
-				location.assign("/explore");
-				return true;
-			} else {
-				return false;
+				if (resp.status === 200) {
+					router.push("/explore");
+					return true;
+				} else {
+					return false;
+				}
 			}
-		}
-	};
+		};
+		checkCookie();
+	}, [cookie]);
 	const subheaders = [
 		"Huelet makes creators feel safe, connected and supported. And audiences feel like they belong.",
 		"Building a video platform for everyone.",
@@ -37,7 +42,6 @@ const Home: NextPage = () => {
 		"Connect with the creators you love and support them directly by rewarding them when you watch, like and share their videos.",
 		"Better moderation, better privacy, better content on the video platform for humans with Huelet.",
 	];
-	checkCookie();
 	return (
 		<div id="klausen">
 			<div className={styles.container}>
@@ -45,7 +49,7 @@ const Home: NextPage = () => {
 					<h1
 						className={`${styles.mainContentText} ${styles.text} ${styles.textBig}`}
 					>
-            The video platform for humans.
+						The video platform for humans.
 					</h1>
 					<div className={styles.divider}></div>
 					<p
