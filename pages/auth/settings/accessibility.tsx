0a111e5a112df2121e66f9418b4b7ddb67972092
Levi R <levir620@skiff.com>
2022-08-21 19:08:48 -0700

    ♿️ add accessibility settings

--- pages/auth/settings/accessibility.tsx
+++ pages/auth/settings/accessibility.tsx
@@ -4,67 +4,276 @@ import { useState } from "react";
 import { useCookies } from "react-cookie";
 import styles from "../../../styles/Settings.module.css";
 import { useToggle } from "@mantine/hooks";
-import { Switch } from "@mantine/core";
+import { Slider, Switch } from "@mantine/core";
 import { Header } from "../../../components/header";
 import { Card } from "@huelet/foundation-ui";
 
 const Accessibility: NextPage = () => {
-	const [loading, setLoading] = useState(true);
 	const [cookie, setCookie] = useCookies(["_hltoken"]);
-	const [soundCookie, setSoundCookie] = useCookies(["_hlsound"]);
 	const [username, setUsername] = useState("");
 
-	/* settings */
-	const [soundsOn, setSoundsOn] = useToggle(true, [true, false]);
+	const [sounds, toggleSounds] = useToggle(true, [true, false]);
+	const [captions, toggleCaptions] = useToggle(true, [true, false]);
+	const [motion, toggleMotion] = useToggle(true, [true, false]);
+	const [highContrast, toggleHighContrast] = useToggle(true, [true, false]);
+	const [invertColors, toggleInvertColors] = useToggle(true, [true, false]);
+	const [grayscale, toggleGrayscale] = useToggle(true, [true, false]);
+	const [autoplay, toggleAutoplay] = useToggle(true, [true, false]);
+	const [zoom, setZoom] = useState(1);
 
-	const checkCookie = () => {
-		const token = cookie._hltoken;
-		if (token) {
-			fetch("https://api.huelet.net/auth/token", {
+	React.useEffect(() => {
+		fetch("https://api.huelet.net/auth/token", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${cookie._hltoken}`,
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => setUsername(data.username));
+	});
+
+	React.useEffect(() => {
+		fetch(
+			`https://api.huelet.net/auth/accessibility?username=${username}&options=*`,
+			{
 				method: "GET",
-				mode: "cors",
-				cache: "no-cache",
 				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${token}`,
+					Authorization: `Bearer ${cookie._hltoken}`,
 				},
-			})
-				.then((res) => res.json())
-				.then((res) => {
-					if (res.response === "Success!") {
-						setUsername(res.username);
-					} else {
-						console.log("error: ", res);
-					}
-				});
-		}
-	};
-	if (loading === true) {
-		checkCookie();
-	} else {
-		null;
-	}
+			}
+		)
+			.then((resp) => resp.json())
+			.then((data) => {
+				const {
+					sounds,
+					captions,
+					motion,
+					highContrast,
+					zoom,
+					invertColors,
+					grayscale,
+					autoplay,
+				} = data.response;
+				toggleSounds(sounds);
+				toggleCaptions(captions);
+				toggleMotion(motion);
+				toggleHighContrast(highContrast);
+				setZoom(zoom);
+				toggleInvertColors(invertColors);
+				toggleGrayscale(grayscale);
+				toggleAutoplay(autoplay);
+			});
+	}, [username, cookie._hltoken]);
+
 	return (
 		<div id="klausen">
 			<Header username="" />
-			<Card full={true}>
-				<div className="mainText">
-					<h2 className={styles.mainText}>Accessibility</h2>
-				</div>
-				<div className={styles.divider}></div>
-				<div className={styles.profile}>
-					<div className={styles.profileOptionItem}>
-						<Switch
-							label="Sounds"
-							size="md"
-							color="violet"
-							checked={soundsOn}
-							onChange={(event) => {
-								setSoundsOn(event.currentTarget.checked);
-								setSoundCookie("_hlsound", event.currentTarget.checked);
-							}}
-						/>
-					</div>
+			<Card full={true} title="Accessibility">
+				<Switch
+					label="Sounds"
+					size="md"
+					color="violet"
+					checked={sounds}
+					onChange={() => {
+						toggleSounds();
+						localStorage.setItem("sounds", sounds.toString());
+						fetch(
+							`https://api.huelet.net/auth/accessibility?username=${username}`,
+							{
+								method: "PATCH",
+								headers: {
+									"Content-Type": "application/json",
+									Authorization: `Bearer ${cookie._hltoken}`,
+								},
+								body: JSON.stringify({
+									settings: {
+										sounds,
+									},
+								}),
+							}
+						);
+					}}
+				/>
+				<Switch
+					label="Captions"
+					size="md"
+					color="violet"
+					checked={captions}
+					onChange={() => {
+						toggleCaptions();
+						localStorage.setItem("captions", captions.toString());
+						fetch(
+							`https://api.huelet.net/auth/accessibility?username=${username}`,
+							{
+								method: "PATCH",
+								headers: {
+									"Content-Type": "application/json",
+									Authorization: `Bearer ${cookie._hltoken}`,
+								},
+								body: JSON.stringify({
+									settings: {
+										captions,
+									},
+								}),
+							}
+						);
+					}}
+				/>
+				<Switch
+					label="Motion"
+					size="md"
+					color="violet"
+					checked={motion}
+					onChange={() => {
+						toggleMotion();
+						localStorage.setItem("motion", motion.toString());
+						fetch(
+							`https://api.huelet.net/auth/accessibility?username=${username}`,
+							{
+								method: "PATCH",
+								headers: {
+									"Content-Type": "application/json",
+									Authorization: `Bearer ${cookie._hltoken}`,
+								},
+								body: JSON.stringify({
+									settings: {
+										motion,
+									},
+								}),
+							}
+						);
+					}}
+				/>
+				<Switch
+					label="High Contrast"
+					size="md"
+					color="violet"
+					checked={highContrast}
+					onChange={() => {
+						toggleHighContrast();
+						localStorage.setItem("highContrast", highContrast.toString());
+						fetch(
+							`https://api.huelet.net/auth/accessibility?username=${username}`,
+							{
+								method: "PATCH",
+								headers: {
+									"Content-Type": "application/json",
+									Authorization: `Bearer ${cookie._hltoken}`,
+								},
+								body: JSON.stringify({
+									settings: {
+										highContrast,
+									},
+								}),
+							}
+						);
+					}}
+				/>
+				<Switch
+					label="Invert Colors"
+					size="md"
+					color="violet"
+					checked={invertColors}
+					onChange={() => {
+						toggleInvertColors();
+						localStorage.setItem("invertColors", invertColors.toString());
+						fetch(
+							`https://api.huelet.net/auth/accessibility?username=${username}`,
+							{
+								method: "PATCH",
+								headers: {
+									"Content-Type": "application/json",
+									Authorization: `Bearer ${cookie._hltoken}`,
+								},
+								body: JSON.stringify({
+									settings: {
+										invertColors,
+									},
+								}),
+							}
+						);
+					}}
+				/>
+				<Switch
+					label="Grayscale"
+					size="md"
+					color="violet"
+					checked={grayscale}
+					onChange={() => {
+						toggleGrayscale();
+						localStorage.setItem("grayscale", grayscale.toString());
+						fetch(
+							`https://api.huelet.net/auth/accessibility?username=${username}`,
+							{
+								method: "PATCH",
+								headers: {
+									"Content-Type": "application/json",
+									Authorization: `Bearer ${cookie._hltoken}`,
+								},
+								body: JSON.stringify({
+									settings: {
+										grayscale,
+									},
+								}),
+							}
+						);
+					}}
+				/>
+				<Switch
+					label="Autoplay"
+					size="md"
+					color="violet"
+					checked={autoplay}
+					onChange={() => {
+						toggleAutoplay();
+						localStorage.setItem("autoplay", autoplay.toString());
+						fetch(
+							`https://api.huelet.net/auth/accessibility?username=${username}`,
+							{
+								method: "PATCH",
+								headers: {
+									"Content-Type": "application/json",
+									Authorization: `Bearer ${cookie._hltoken}`,
+								},
+								body: JSON.stringify({
+									settings: {
+										autoplay,
+									},
+								}),
+							}
+						);
+					}}
+				/>
+				<div className={styles.zoom}>
+					<div className={styles.zoomLabel}>Zoom</div>
+					<Slider
+						min={0.5}
+						max={2}
+						step={0.5}
+						value={zoom}
+						onChangeEnd={(e) => {
+							console.log(e);
+
+							setZoom(parseFloat(e.toString()));
+							localStorage.setItem("zoom", e.toString());
+							fetch(
+								`https://api.huelet.net/auth/accessibility?username=${username}`,
+								{
+									method: "PATCH",
+									headers: {
+										"Content-Type": "application/json",
+										Authorization: `Bearer ${cookie._hltoken}`,
+									},
+									body: JSON.stringify({
+										settings: {
+											zoom,
+										},
+									}),
+								}
+							);
+						}}
+					/>
 				</div>
 			</Card>
 		</div>
