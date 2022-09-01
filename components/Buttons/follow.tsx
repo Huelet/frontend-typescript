ad7e4f95b75fc878930365cd1e1b0b823a56fc4a
Levi R <levir620@skiff.com>
2022-09-01 14:46:47 -0700

    ✨ add follow button

--- components/Buttons/follow.tsx
+++ components/Buttons/follow.tsx
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import * as React from "react";
-import { jsx, css } from "@emotion/react";
-import { Add } from "@fdn-ui/icons-react";
+import { jsx, css, keyframes } from "@emotion/react";
+import { Add, Close } from "@fdn-ui/icons-react";
+import { useRouter } from "next/router";
 
 export interface FollowProps {
 	username: string;
@@ -9,6 +10,23 @@ export interface FollowProps {
 }
 
 export const Follow = ({ username, chonky }: FollowProps) => {
+	const [followed, setFollowed] = React.useState(false);
+
+	React.useEffect(() => {
+		if (typeof username !== "undefined" && localStorage.getItem("username")) {
+			fetch(`https://api.huelet.net/users/info/following?username=${username}`)
+				.then((res) => res.json())
+				.then((data) => {
+					console.log(data);
+
+					if (data.following.includes(localStorage.getItem("username"))) {
+						setFollowed(true);
+					}
+				});
+		}
+	}, [username]);
+
+	const router = useRouter();
 	return (
 		<div
 			css={{
@@ -22,12 +40,52 @@ export const Follow = ({ username, chonky }: FollowProps) => {
 				padding: "0.75em",
 				cursor: "pointer",
 			}}
+			onClick={async () => {
+				if (localStorage.getItem("token")) {
+					const resp = await fetch(
+						`https://api.huelet.net/users/interact/follow?username=${localStorage.getItem(
+							"username"
+						)}&userToFollow=${username}`,
+						{
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+								Authorization: `Bearer ${localStorage.getItem("token")}`,
+							},
+						}
+					);
+
+					if (resp.status === 200) {
+						setFollowed(!followed);
+					}
+				} else {
+					router.push("/auth/in");
+				}
+			}}
 		>
-			<Add fill={"white"} />
+			<Add
+				fill={"white"}
+				css={{
+					transform: followed ? "rotate(45deg)" : "rotate(0deg)",
+					animation: `${
+						followed
+							? keyframes({
+									from: { transform: "rotate(0deg)" },
+									to: { transform: "rotate(405deg)" },
+							  })
+							: keyframes({
+									from: { transform: "rotate(405deg)" },
+									to: { transform: "rotate(0deg)" },
+							  })
+					} 1000ms linear`,
+					userSelect: "none",
+				}}
+			/>
 			<p
 				css={{
 					fontSize: "1.2em",
 					fontWeight: "bold",
+					userSelect: "none",
 				}}
 			>
 				Follow
