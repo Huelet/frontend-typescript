3c7b3fecb027b5b192a459a1c9651d1813cd7d3e
Levi R <levir620@skiff.com>
2022-09-02 23:03:53 -0700

    🧑‍💻 create useFetch hook!

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,16 +1,33 @@
 export interface FetchProps {
 	url: string;
 	method?: string;
-	body?: string;
+	body?: object;
 	headers?: { [key: string]: string };
 }
 
-export const fetcher = ({ url, method = "GET", body, headers }: FetchProps) =>
-	fetch(url, {
-		method: method,
-		body: JSON.stringify(body),
-		headers: {
-			"Content-Type": "application/json",
-			...headers,
-		},
-	}).then((res) => res.json());
+export const useFetch = async ({
+	url,
+	method = "GET",
+	body,
+	headers,
+}: FetchProps) => {
+	let error = false;
+	let loading = true;
+	try {
+		const res = await fetch(url, {
+			method: method,
+			body: JSON.stringify(body),
+			headers: {
+				"Content-Type": "application/json",
+				...headers,
+			},
+		});
+		const data = await res.json();
+		loading = false;
+		return [data, error, loading];
+	} catch (err) {
+		error = true;
+		loading = false;
+		return [null, error, loading];
+	}
+};
